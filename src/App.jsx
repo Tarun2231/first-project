2258550473dfcd3b796cb8418a556460bcb4f9db
Tarun2231 <milaritarun@gmail.com>
2024-06-28 18:10:05 +0530

    event handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import Navbar from "./components/Navbar"
+import { useState } from "react";
 
 const App = () => {
+
+  // let data ="girl";
+
+// event handling
+
+const [x,setx] =useState(0);
+
+const btnclick=()=>{
+  console.log("clicked");
+  setx(x+1);
+  console.log(x);
+  }
+
   return (
     <div>
-      <Navbar/>
+      {x}
+      {/* {data==="boy"?<h1>boy</h1>:<h1>girl</h1>} */}
+
+<button onClick={()=>{btnclick()}}>click me</button>
+
     </div>
   )
 }
